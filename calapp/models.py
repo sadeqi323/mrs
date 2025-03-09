59ca4b10f752e0e4df01166046704f51f5c35a4a
mrsadeqi <sadeqi323@google.com>
2025-03-09 23:34:32 +0330

    calery in and out

--- calapp/models.py
+++ calapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CalIn(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class CalOut(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
